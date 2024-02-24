acf1fcde9c4d2d592a0360e2289b58d2a54b9a65
Jason Lassiter <jlassiter0522@gmail.com>
2024-02-24 22:43:14 +0000

    Commit

---
+++ demo/src/main/java/com/example/demo/domain/XkcdComic.java
@@ -0,0 +1,20 @@
+package com.example.demo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class XkcdComic {
+    private String month;
+    private int num;
+    private String link;
+    private String year;
+    private String news;
+    private String safe_title;
+    private String transcript;
+    private String alt;
+    private String img;
+    private String title;
+    private String day;
+}
