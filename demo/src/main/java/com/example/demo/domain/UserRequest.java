8ccb91dd8f53e6b360d559b2c3f3a41b26e18f5d
Jason Lassiter <jlassiter0522@gmail.com>
2024-03-03 23:49:43 +0000

    Commit

--- demo/src/main/java/com/example/demo/domain/UserRequest.java
+++ demo/src/main/java/com/example/demo/domain/UserRequest.java
@@ -1,29 +1,36 @@
 package com.example.demo.domain;
-import jakarta.validation.constraints.Max;
+/*import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+*/
 import lombok.Getter;
 import lombok.Setter;
-
 @Getter
 @Setter
 public class UserRequest {
 
     //creating a validator
     //regex checks over strings to see if it contains the patten
+    /* 
     @NotNull(message = "first name cannot be null")
     @NotBlank(message = "first name cannot be empty")
+    
     @Pattern(regexp ="[a-zA-Z]*", message = "first name can only be alphabetic")
+    */
     private String firstName;
+    /* 
     @NotNull(message = "last name cannot be null")
     @NotBlank(message = "last name cannot be empty")
     @Pattern(regexp ="[a-zA-Z]*")
+    */
     private String lastName;
     private String phone;
     private String email;
+    /* 
     @Min(value = 16, message= "age less than 16")
     @Max(value = 120, message = "age cannot be more than 120")
+    */
     private int age;
 }
--- demo/src/main/react/App.js
+++ demo/src/main/react/App.js
@@ -1,48 +1,14 @@
 import React, {Component} from "react";
 import ReactDom from "react-dom";
+import AppContainer from "/containers/AppContainer"
 export class App extends Component{
 
     render(){
         return(
-        <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">Navbar</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        <h1>Hello, Cole!</h1>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-    </>
-        )
-    }
+        <AppContainer />
+    )
+  }
 }
 
 ReactDom.render(<App />, document.querySelector("#app"));
 
-    
