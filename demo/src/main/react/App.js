a09fa199726e08af9185a84b3bd090625eed611f
Jason Lassiter <jlassiter0522@gmail.com>
2024-03-04 02:31:30 +0000

    push

--- demo/src/main/react/App.js
+++ demo/src/main/react/App.js
@@ -2,13 +2,48 @@ import React, {Component} from "react";
 import ReactDom from "react-dom";
 import AppContainer from "/containers/AppContainer"
 export class App extends Component{
+    constructor(props){
+      super(props)
+      this.state = {
+        object:"Ball",
+        count: 0
+      }
+    }
+
 
     render(){
+      //const object = "Ball"
+        const { object, count } = this.state
+        const increment = () => {
+        this.setState ({count: this.state.count + 1 })
+      }
         return(
-        <AppContainer />
+        <AppContainer
+        name = "Cole"
+        color = "Blue"
+        object = {this.state.object}
+        count = {count}
+        increment = {increment}
+        />
     )
   }
 }
 
+
+
 ReactDom.render(<App />, document.querySelector("#app"));
 
+// components can have props and states 
+//props -> passed down from parent
+//states -> owned by the component
+//prop and state changes in the component trigger updates to the DOM
+    //life cycle methods -> 
+  /*componentDidMount, = runs once when the component loads into DOM
+        API Calls are best here, setState
+    componentDidUpdate, = runs at every props or state change
+      prevProps and preState and compare to current
+    componentWillUnmount
+      Clean up of subscriptions (Listoners)
+    shouldComponentUpdate = rus at every props or state change
+      defaults to true generally checking previous props/states to new ones. if return false component will not update.
+    */
--- demo/src/main/react/containers/AppContainer.js
+++ demo/src/main/react/containers/AppContainer.js
@@ -1,10 +1,15 @@
-import React from 'react'
-const AppContainer = () => {
+import React, {useEffect, useState} from 'react'
+const AppContainer = (props) => {
+    const {name, color, object, count, increment} = props
+    const [count2, setCount2] = useState(0)
+
     return(
         <>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">
+              {name} {count2}
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
@@ -14,10 +19,10 @@ const AppContainer = () => {
                   <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Features</a>
+                  <a class="nav-link" href="#" onClick={() => setCount2(count2 +1)}>{count2}</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">Pricing</a>
+                  <a class="nav-link" href="#" onClick={() => increment }>{count}</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link disabled" aria-disabled="true">Disabled</a>
@@ -38,3 +43,23 @@ const AppContainer = () => {
       </>
     )
 }
+export default AppContainer
+    /*component did mount
+ useEffect(() =>{
+
+
+//  },[])
+// componentDidUpdate
+  useEffect(() =>{
+
+
+  },[count]) //or instead of props we can use specific veriable names
+
+  //componentWillUnmount
+  useEffect(() =>{
+    return function cleanUp(){
+
+    }
+
+  },)
+    */
