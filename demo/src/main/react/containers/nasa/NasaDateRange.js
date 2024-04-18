5696fbb364118612842736ba652eda5e9843a3e2
Jason Lassiter <jlassiter0522@gmail.com>
2024-04-18 16:17:22 +0000

    2

--- demo/src/main/react/containers/nasa/NasaDateRange.js
+++ demo/src/main/react/containers/nasa/NasaDateRange.js
@@ -10,6 +10,7 @@ const NasaDateRange = () => {
     const updateStart = useStore((state) => state.updateStartDate)
     const updateEnd = useStore((state) => state.updateEndDate)
     const fetchRange = useStore((state) =>state.fetchRange)
+    const.log(apiArray)
     return (
 
         <>
